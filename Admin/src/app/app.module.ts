9e36f0ff4f5731b9f1e16bca3381c16279834dd2
Jose Guillermo <jguillermo@outlook.com>
2018-01-07 22:23:49 -0500

    creando modulos

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -4,8 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import {NopagefoundComponent} from "./shared/nopagefound/nopagefound.component";
 
 
 @NgModule({
@@ -13,7 +17,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
